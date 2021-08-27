356578a34d0742c10632472450fd38d8bfd713d1
Firdavs Murodov <firdavs.murodov@gmail.com>
2021-08-27 12:48:58 +0500

    grpc

--- storage.go
+++ storage.go
@@ -7,14 +7,14 @@ import (
 )
 
 // get url from postgres
-func getUrl(db *sql.DB, hash string) (string, error) {
+func Get(db *sql.DB, hash string) (string, error) {
 	var url string
 	err := db.QueryRow("SELECT url FROM surl WHERE hash = $1", hash).Scan(&url)
 	return url, err
 }
 
 // add url to postgres
-func addUrl(db *sql.DB, url string) (string, error) {
+func Add(db *sql.DB, url string) (string, error) {
 
 	hash := random.RandString()
 	_, err := db.Exec("INSERT INTO surl (url,hash) VALUES ($1,$2)", url, hash)
