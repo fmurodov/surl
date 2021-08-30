a292bc720093eb35b27e9fa0d9f4dfac179be7f1
Firdavs Murodov <firdavs.murodov@gmail.com>
2021-08-30 16:38:51 +0500

    test getenv

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -1,3 +1,17 @@
 package main
 
+import (
+	"testing"
+)
+
 // TODO: Test the server.
+
+// test getEnv function
+func TestGetEnv(t *testing.T) {
+	var env = "TEST"
+	var expected = "defautval"
+	var actual = getEnv(env, expected)
+	if actual != expected {
+		t.Errorf("getEnv(%s) = %s; want %s", env, actual, expected)
+	}
+}
